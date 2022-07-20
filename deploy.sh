a5a9d7db32de9eea12e6c177083ace7880d65a0b
문찬웅 <intajon@gmail.com>
2022-07-20 10:10:51 +0900

    rebuilding site 2022년 7월 20일 수요일 10시 10분 51초 KST

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 # hugo -t <여러분의 테마>
-hugo -t blonde
+hugo -t stack
 
 # Go To Public folder, sub module commit
 cd public
@@ -19,7 +19,7 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin main
+git push origin main -f
 
 # Come Back up to the Project Root
 cd ..
@@ -34,4 +34,4 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-git push origin main
+git push origin main -f
